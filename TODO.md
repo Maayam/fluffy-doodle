a306f7237d80b8ea2c9c760ce1ef48327a7fc034
Maayam <antoine.devos.francis@gmail.com>
2016-08-25 22:05:55 +0200

    added rough map template
    -map displays properly thanks to leaflet
    Need to use proper licence later though (curently using OSM for tests)

---
+++ TODO.md
@@ -0,0 +1,6 @@
+I'LL DO IT LATER
+=================
+
+-set mapView acording to users location
+-afficher l'échelle des distances
+-bouger les boutons zoom/dezoom
---
+++ web/js/map.js
@@ -0,0 +1,28 @@
+var map;
+var ajaxRequest;
+var plotlist;
+var plotlayers=[];
+
+function initmap() { //loads the map
+	// set up the map
+	map = new L.Map('userMap');
+
+	// create the tile layer with correct attribution
+	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+	var osm = new L.TileLayer(osmUrl, {minZoom: 4, maxZoom: 19, attribution: osmAttrib});		
+
+	// start the map in South-East England
+	map.setView(new L.LatLng(51.3, 0.7),9);
+	map.addLayer(osm);
+}
+
+function getXmlHttpObject() { 
+	if (window.XMLHttpRequest) { return new XMLHttpRequest(); }
+	if (window.ActiveXObject)  { return new ActiveXObject("Microsoft.XMLHTTP"); }
+	return null;
+}
+
+//more at https://switch2osm.org/using-tiles/getting-started-with-leaflet/
+
+initmap();
