59ad26d1af6ed63f9517280ef3e8ec38482e3291
Konosprod <konosprod@free.fr>
2016-10-05 13:08:58 +0200

    Disable scroll to zoom and draggable map for plot view map
    Signed-off-by: Konosprod <konosprod@free.fr>

--- web/js/plotview.js
+++ web/js/plotview.js
@@ -1,23 +1,30 @@
 var map;
 
 function initmap() {
-	map = new L.Map('map-plot');
+
+	var options = {
+		scrollWheelZoom:true,
+		dragging:false,
+	};
+	
+	map = new L.Map('map-plot', options);
 
 	var lat = $('#plot-header').data('lat');
 	var lng = $('#plot-header').data('lng');
 		
 	var home = new L.LatLng(lat, lng);
 
-	// create the tile layer with correct attribution
 	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
 	var osm = new L.TileLayer(osmUrl, {minZoom: 4, maxZoom: 18, attribution: osmAttrib});		
+	var optionsMarker = {
+		clickable: false,
+	};
 
-	// start the map in South-East England
-	map.setView(home ,12);
+	map.setView(home, 12);
 	map.addLayer(osm);
 	
-	L.marker(home).addTo(map);
+	L.marker(home, optionsMarker).addTo(map);
 }
 
 initmap();
